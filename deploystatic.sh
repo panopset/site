d882b6c4729727c9af182b8558ea90f456be6369
Karl Dinwiddie <karldinwiddie@gmail.com>
2022-11-18 11:25:30 -0800

    .

--- deploystatic.sh
+++ deploystatic.sh
@@ -1,2 +1,2 @@
-cp -r raw/* /var/www/html/
-scp -r raw/* $PANOPSET_SITE_USR@$PANOPSET_SITE_NAME:/var/www/$PANOPSET_SITE_DN/html/
+rsync -r raw/* /var/www/html/
+rsync -r raw/* $PANOPSET_SITE_USR@$PANOPSET_SITE_NAME:/var/www/$PANOPSET_SITE_DN/html/
