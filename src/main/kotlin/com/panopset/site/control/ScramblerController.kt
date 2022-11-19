98edaa3d4570849526c241995ee2b4d103178c5a
Karl Dinwiddie <karldinwiddie@gmail.com>
2022-11-19 08:28:28 -0800

    .

---
+++ src/main/kotlin/com/panopset/site/control/ScramblerController.kt
@@ -0,0 +1,53 @@
+package com.panopset.site.control
+
+import com.panopset.compat.Stringop
+import com.panopset.compat.TextScrambler
+import com.panopset.web.ScramblerInput
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import javax.servlet.http.HttpServletResponse
+
+@Controller
+class ScramblerController {
+
+
+    @GetMapping(*["/scrambler", "/scrambler.htm", "/scrambler.html"])
+    fun scrambler(model: Model?, response: HttpServletResponse?): String? {
+        return "scrambler"
+    }
+
+    @PostMapping("/scramble")
+    fun scramble(@RequestBody si: ScramblerInput, response: HttpServletResponse?): ResponseEntity<String?>? {
+        val result: String
+        val text: String = si.text ?: ""
+        val koi: String = si.koi ?: ""
+        val passphrase: String = si.passphrase ?: ""
+        val k = Stringop.parseInt(koi, 10000)
+        result = try {
+            TextScrambler().withKeyObtentionIters(k).encrypt(passphrase, text)
+        } catch (e: Exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.message)
+        }
+        return ResponseEntity.ok(result)
+    }
+
+    @PostMapping("/unscramble")
+    fun unscramble(@RequestBody si: ScramblerInput, response: HttpServletResponse?): ResponseEntity<String?>? {
+        val result: String
+        val text: String = si.text ?: ""
+        val koi: String = si.koi ?: ""
+        val passphrase: String = si.passphrase ?: ""
+        val k = Stringop.parseInt(koi, 10000)
+        result = try {
+            TextScrambler().withKeyObtentionIters(k).decrypt(passphrase, text)
+        } catch (e: Exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.message)
+        }
+        return ResponseEntity.ok(result)
+    }
+}
---
+++ src/main/kotlin/com/panopset/web/ScramblerInput.kt
@@ -0,0 +1,7 @@
+package com.panopset.web
+
+class ScramblerInput {
+    var text: String? = null
+    var koi: String? = null
+    var passphrase: String? = null
+}
