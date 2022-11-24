64e57e0b98e24f14d05bc251a0284a8269de298b
Karl Dinwiddie <karldinwiddie@gmail.com>
2022-11-24 04:55:53 -0800

    .

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,5 @@ set -e
 set -o pipefail
 ./deploylocal.sh
 scp ./build/libs/site.jar $PANOPSET_SITE_USR@$PANOPSET_SITE_NAME:/home/$PANOPSET_SITE_USR/
-rsync -r /var/www/html $PANOPSET_SITE_USR@$PANOPSET_SITE_NAME:/var/www/$PANOPSET_SITE_DN/
+rsync -ravuzh /var/www/html $PANOPSET_SITE_USR@$PANOPSET_SITE_NAME:/var/www/$PANOPSET_SITE_DN/
 echo $PANOPSET_SITE_PWD | ssh -tt $PANOPSET_SITE_NAME "sudo reboot 0"
-
