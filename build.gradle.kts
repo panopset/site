9177f62edb96afcb98b1fb6f339275ff717fa87c
Karl Dinwiddie <karldinwiddie@gmail.com>
2023-07-18 07:17:13 -0700

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 // Also update pom.xml
 
 plugins {
-	id("org.springframework.boot") version "2.7.5"
-	id("io.spring.dependency-management") version "1.0.14.RELEASE"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+	id("org.springframework.boot") version "3.1.1"
+	id("io.spring.dependency-management") version "1.1.1"
+	kotlin("jvm") version "1.9.0"
+	kotlin("plugin.spring") version "1.9.0"
 }
 
 group = "com.panopset"
-version = "1.2.3"
+version = "1.2.4"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
@@ -28,12 +28,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.22")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.22")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
 	implementation("org.springframework.session:spring-session-core")
-	implementation("redis.clients:jedis:4.3.1")
+	implementation("redis.clients:jedis:4.4.3")
 	implementation("org.springframework.boot:spring-boot-configuration-processor")
-	implementation("com.panopset:flywheel:1.2.3")
+	implementation("com.panopset:flywheel:1.2.4")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
